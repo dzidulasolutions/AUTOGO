4fa1a981f1d57c608c089a5183e8016b08151e59
dzidula <dzidulasolutions@gmail.com>
2026-08-07 21:43:02 +0000

    feat: ajout documentation swagger sur api/docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true }); // bufferLogs: true permet de stocker les logs dans un buffer avant que le logger ne soit initialisé, ce qui est utile pour capturer les logs générés pendant le démarrage de l'application.
   app.useLogger(app.get(Logger));
+  const config = new DocumentBuilder()
+    .setTitle('AuTogo API')
+    .setDescription('API backend pour la plateforme de microfinance AuTogo')
+    .setVersion('0.1')
+    .addBearerAuth() // pour l'authentification JWT qu'on ajoutera plus tard
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
